2104a99976bed41a74d629cf6b011eea1ca4163e
Wissam <wissamayass.lb@gmail.com>
2022-06-13 21:49:06 +0300

    last-touch

--- src/components/borrowedBy.js
+++ src/components/borrowedBy.js
@@ -9,7 +9,11 @@ import "./CSS files/book.css";
 
 const BorrowedBy = ({ email, bookTitle, bookImage, bookId, dateTo }) => {
   let expiryDate = new Date(dateTo);
+  // console.log(expiryDate.getTime());
+  const msPerDay = 24 * 60 * 60 * 1000;
   let today = new Date();
+  let daysLeft = (expiryDate.getTime() - today.getTime()) / msPerDay; //Number of milliseconds per day
+  // console.log(Math.round(daysLeft));
   const holdClick = async () => {
     const res = await axios
       .put(`http://localhost:3002/books/update/${bookId}`, {
@@ -76,6 +80,22 @@ const BorrowedBy = ({ email, bookTitle, bookImage, bookId, dateTo }) => {
       </IconButton>
       <div className="card-content">
         <div className="card-body">
+          {!expiry && (
+            <h3 className="return">
+              <h1>{Math.round(daysLeft)}</h1>
+              {Math.round(daysLeft) == 1 ? <p>day left</p> : <p>days left</p>}
+            </h3>
+          )}
+          {expiry && (
+            <h3 className="return bg-red">
+              <h1>{Math.round(daysLeft)}</h1>
+              {Math.round(daysLeft) == -1 ? (
+                <p>day behind</p>
+              ) : (
+                <p>days behind</p>
+              )}
+            </h3>
+          )}
           <h2>Book: {bookTitle}</h2>
           <img src={bookImage} alt="" />
           <div className={expiry ? "bg-red" : ""}>
